a58ea0071c685776f033e73356bd5078e69438ac
Aleksei Kostiukov <a.kostiukov2011@yandex.ru>
2024-11-27 21:27:39 +0300

    реализация отображения карточек на главной странице

---
+++ src/components/app.tsx
@@ -0,0 +1,11 @@
+import MainPage from "../pages/main-page-component/main";
+
+type MainPageProps = {
+  PlacesCount: number;
+}
+function App({PlacesCount}: MainPageProps): JSX.Element {
+  return (
+    <MainPage PlacesCount={PlacesCount} />
+  );
+}
+export default App;
